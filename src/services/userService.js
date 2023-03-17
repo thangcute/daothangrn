016e97aa0a489ea12b11657e59a89c7d5d1e445f
dao van thang <daovanthang083@gmail.com>
2023-03-17 18:08:07 +0700

    CRUD complete

--- src/services/userService.js
+++ src/services/userService.js
@@ -106,25 +106,25 @@ let createNewUser = (data) => {
       if (check === true) {
         resolve({
           errCode: 1,
-          message: "Your email is already in used, plz try another email!",
+          errMessage: "Your email is already in used, plz try another email!",
+        });
+      } else {
+        let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+        await db.User.create({
+          email: data.email,
+          password: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          phonenumber: data.phonenumber,
+          gender: data.gender === "1" ? true : false,
+          roleId: data.roleId,
+        });
+        resolve({
+          errCode: 0,
+          message: "OK",
         });
       }
-      let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-      await db.User.create({
-        email: data.email,
-        password: hashPasswordFromBcrypt,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        address: data.address,
-        phonenumber: data.phonenumber,
-        gender: data.gender === "1" ? true : false,
-        roleId: data.roleId,
-      });
-
-      resolve({
-        errCode: 0,
-        message: "OK",
-      });
     } catch (error) {
       reject(error);
     }
