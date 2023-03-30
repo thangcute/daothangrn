6703915288abba65533fecb2afc33614952e99d0
dao van thang <daovanthang083@gmail.com>
2023-03-30 13:11:49 +0700

    update

--- src/server.js
+++ src/server.js
@@ -37,8 +37,14 @@ app.use(function (req, res, next) {
 
 //config app
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: "50mb",
+    parameterLimit: 50000,
+  })
+);
 
 viewEngine(app);
 initWebRoutes(app);
