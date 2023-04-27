542fd0d8a59dd38ab9cdab52d7d53219bab7db41
dao van thang <daovanthang083@gmail.com>
2023-04-27 09:54:52 +0700

    update

--- src/route/web.js
+++ src/route/web.js
@@ -23,6 +23,8 @@ let initWebRoutes = (app) => {
   router.get("/api/allcode", userController.getAllCode);
 
   router.get("/api/top-doctor-home", doctorController.getTopDoctorHome);
+  router.get("/api/get-all-doctors", doctorController.getAllDoctors);
+  router.post("/api/save-info-doctors", doctorController.postInfoDoctor);
 
   router.get("/thang", (req, res) => {
     return res.send("hello thắng");
